f81634a0b82b42fc8df5176ca1180c23267dbdba
Matheus Calixto <39939428+mthcalixto@users.noreply.github.com>
2023-01-01 20:09:27 -0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+}
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Fixes packages that depend on fs/module module
+    if (!isServer) {
+      config.node = { fs: "empty", module: "empty" };
+    }
+
+    return config;
+  },
+  nextConfig
+};
+
+module.exports = nextConfig
